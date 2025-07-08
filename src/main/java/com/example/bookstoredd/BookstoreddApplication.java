1e559401e029ec8e70ad4dd2345476ed44d8bbb5
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2025-07-07 22:06:45 -0300

    initia

---
+++ src/main/java/com/example/bookstoredd/BookstoreddApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoredd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookstoredd/BookstoreddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookstoredd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
